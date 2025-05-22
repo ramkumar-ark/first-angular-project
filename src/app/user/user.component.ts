bc6af44e8a0296cce3a0541287ab180b3e510b4d
Ram Kumar <ramgambit@gmail.com>
2025-05-22 08:32:02 +0530

    implemented dynamic user data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
 }
