777b61634c2ae0fbca572282658b9314b9542c00
Ram Kumar <ramgambit@gmail.com>
2025-05-23 18:34:27 +0530

    basic task component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,27 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+
+export type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = signal(DUMMY_USERS);
+  selectedUser = signal<User | null>(null);
 
   onSelectUser(userId: string) {
-    console.log('user selected ' + userId);
+    this.selectedUser.set(
+      this.users().find((user) => user.id === userId) || null
+    );
   }
 }
