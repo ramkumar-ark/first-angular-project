ae26555290521641577170d742a104eadef39119
Ram Kumar <ramgambit@gmail.com>
2025-05-23 21:08:29 +0530

    created and implemented task component

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,19 @@
+import { Component, input } from '@angular/core';
+
+export interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+export class TaskComponent {
+  task = input.required<Task>();
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
+import { Task, TaskComponent } from './task/task.component';
+import { dummyTasks } from '../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   userName = input.required<string>();
+  userId = input.required<string>();
+  tasks = computed(() =>
+    dummyTasks.filter((task) => task.userId === this.userId())
+  );
 }
