94af80bf72657b491676defffcac27f2df6b687d
Ram Kumar <ramgambit@gmail.com>
2025-05-24 23:32:37 +0530

    using date pipe for output transformation

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { Task } from './task.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
