749ede2e2d57e34419754ad59e8e7804318465dd
Ram Kumar <ramgambit@gmail.com>
2025-05-25 00:32:57 +0530

    updated TasksService class

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { dummyTasks } from '../dummy-tasks';
 import { Task } from './task/task.model';
+import { NewTask } from './new-task/new-task.model';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
@@ -10,8 +11,9 @@ export class TasksService {
     return this.tasks.filter((task) => task.userId === userId);
   }
 
-  addTask(newTask: Task): void {
-    this.tasks.push(newTask);
+  addTask(newTask: NewTask, userId: string): void {
+    const id = Math.random().toString(36).substring(2, 15);
+    this.tasks.push({ ...newTask, userId, id });
   }
 
   removeTask(taskId: string): void {
