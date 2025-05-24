8332b0f6efb733ac318e523cc99dbc6978fd256c
Ram Kumar <ramgambit@gmail.com>
2025-05-24 21:58:56 +0530

    two way binding with ngModel directive

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,18 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   isVisible = input.required<boolean>();
   close = output<void>();
+  taskTitle = signal('');
+  taskSummary = signal('');
+  taskDueDate = signal<''>('');
   onSubmit(formData: any) {
     // Logic to handle form submission for creating a new task
     console.log('New task submitted:', formData);
