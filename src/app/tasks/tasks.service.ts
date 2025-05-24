be6309005ba7862f6f3667ee694f496a88691131
Ram Kumar <ramgambit@gmail.com>
2025-05-25 01:08:50 +0530

    updated tasks service to use local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -7,6 +7,13 @@ import { NewTask } from './new-task/new-task.model';
 export class TasksService {
   private tasks: Task[] = dummyTasks;
 
+  constructor() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+  }
+
   getUserTasks(userId: string): Task[] {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -14,9 +21,15 @@ export class TasksService {
   addTask(newTask: NewTask, userId: string): void {
     const id = Math.random().toString(36).substring(2, 15);
     this.tasks.push({ ...newTask, userId, id });
+    this.saveTasksToLocalStorage();
   }
 
   removeTask(taskId: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasksToLocalStorage();
+  }
+
+  private saveTasksToLocalStorage(): void {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
